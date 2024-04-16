93366afb31e9dad99748c7d2e18c4a2e8e0e476e
Sufiyaan Tamboli <sufiyaantamboli1983@gmail.com>
2024-04-16 06:37:25 +0530

    Update a.php

--- WT_Solutions/03/a.php
+++ WT_Solutions/03/a.php
@@ -2,7 +2,7 @@
 // Start session
 Session_start();
 // Check if login form has been submi琀琀ed
-If(isset($_POST['submit'])) {
+if (isset($_POST['submit'])) {
     // Get username and password input from user
     $username = $_POST['username'];
     $password = $_POST['password'];
@@ -10,34 +10,34 @@ If(isset($_POST['submit'])) {
     $correct_username = 'myusername';
     $correct_password = 'mypassword';
     // Check if entered username and password are correct
-    If($username == $correct_username && $password == $correct_password) {
-    // Set session variable to mark user as logged in
-    $_SESSION['loggedin'] = true;
-    // Redirect user to welcome page
-    Header('Location: welcome.php');
-    exit;
+    if ($username == $correct_username && $password == $correct_password) {
+        // Set session variable to mark user as logged in
+        $_SESSION['loggedin'] = true;
+        // Redirect user to welcome page
+        Header('Location: welcome.php');
+        exit;
     } else {
-    // Decrement login attempts
-    If(isset($_SESSION['attempts'])) {
-    $_SESSION['attempts']--;
-    } else {
-    $_SESSION['attempts'] = 3;
+        // Decrement login attempts
+        if (isset($_SESSION['attempts'])) {
+            $_SESSION['attempts']--;
+        } else {
+            $_SESSION['attempts'] = 3;
+        }
+        // Display error message if maximum login attempts exceeded
+        if ($_SESSION['attempts'] <= 0) {
+            echo "Maximum login attempts exceeded. Please try again later.";
+        } else {
+            // Display error message
+            echo "Invalid username or password. You have " . $_SESSION['attempts'] . " Attempt(s) left.";
+        }
     }
-    // Display error message if maximum login attempts exceeded
-    If($_SESSION['attempts'] <= 0) {
-    Echo "Maximum login attempts exceeded. Please try again later.";
-} else {
-    // Display error message
-    echo "Invalid username or password. You have ".$_SESSION['attempts']." Attempt(s) left.";
-    }
-    }
-   }
-   ?>
-   
-   <form method="post">
+}
+?>
+
+<form method="post">
     <label for="username">Username:</label>
     <input type="text" id="username" name="username" required><br><br>
     <label for="password">Password:</label>
     <input type="password" id="password" name="password" required><br><br>
     <input type="submit" name="submit" value="Log In">
-   </form>
+</form>
